9353b047d9c1a2c43f263bab478c1bd98d98c9bd
Panagiotis <400362+panosru@users.noreply.github.com>
2020-03-27 20:27:04 +0200

    * .DS_Store: fixes
    * .DS_Store:
    * project.nuget.cache:
    * project.assets.json:
    * i18n.csproj.nuget.cache:
    * i18n.csproj.nuget.g.props:
    * i18n.csproj.nuget.g.targets:
    * i18n.pdb:
    * i18n.dll:
    * i18n.pdb:
    * i18n.dll:
    * i18n.csproj.nuget.dgspec.json:
    * CultureAnchorTagHelper.cs:
    * i18n.Views.dll:
    * i18n.Views.dll:
    * i18n.Views.pdb:
    * i18n.deps.json:
    * i18n.Views.pdb:
    * appsettings.json:
    * i18n.assets.cache:
    * CustomCultureRouteRouteModelConvention.cs:
    * RedirectUnsupportedCultures.cs:
    * i18n.AssemblyInfo.cs:
    * i18n.resources.cs:
    * i18n.resources.cs:
    * i18n.runtimeconfig.json:
    * i18n.resources.dll:
    * i18n.resources.dll:
    * i18n.resources.dll:
    * i18n.resources.dll:
    * i18n.csproj.CopyComplete:
    * i18n.RazorAssemblyInfo.cs:
    * i18n.TagHelpers.input.cache:
    * i18n.genruntimeconfig.cache:
    * i18n.runtimeconfig.dev.json:
    * appsettings.Development.json:
    * i18n.TagHelpers.output.cache:
    * i18n.RazorCoreGenerate.cache:
    * i18n.RazorAssemblyInfo.cache:
    * Index.cshtml.g.cs:
    * i18n.AssemblyInfoInputs.cache:
    * Error.cshtml.g.cs:
    * launchSettings.json:
    * CustomCultureRouteRouteModelConvention.cs:
    * Privacy.cshtml.g.cs:
    * i18n.RazorTargetAssemblyInfo.cs:
    * i18n.csproj.FileListAbsolute.txt:
    * i18n.csproj.GenerateResource.cache:
    * i18n.RazorTargetAssemblyInfo.cache:
    * _ViewStart.cshtml.g.cs:
    * i18n.csprojAssemblyReference.cache:
    * i18n.SharedResource.en-GB.resources:
    * i18n.SharedResource.en-US.resources:
    * i18n.csproj.CoreCompileInputs.cache:
    * _ViewImports.cshtml.g.cs:
    * ChangeCulture.cshtml.g.cs:
    * _Layout.cshtml.g.cs:
    * i18n.StaticWebAssets.xml:
    * i18n.Resources.Pages.Index.en-US.resources:
    * i18n.Resources.Pages.Index.en-GB.resources:
    * i18n.MvcApplicationPartsAssemblyInfo.cache:
    * i18n.Resources.Pages.Privacy.en-US.resources:
    * i18n.Resources.Pages.Privacy.en-GB.resources:
    * i18n.StaticWebAssets.Manifest.cache:
    * _ValidationScriptsPartial.cshtml.g.cs:

---
+++ Localization/CustomCultureRouteRouteModelConvention.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace i18n.Localization
+{
+    public class CustomCultureRouteRouteModelConvention : IPageRouteModelConvention
+    {
+        public void Apply(PageRouteModel model)
+        {
+            (from SelectorModel in model.Selectors
+             select new SelectorModel
+             {
+                 AttributeRouteModel = new AttributeRouteModel
+                 {
+                     Template = AttributeRouteModel.CombineTemplates("/{culture?}", SelectorModel.AttributeRouteModel.Template)
+                 }
+             })
+             .ToList()
+             .ForEach(s => model.Selectors.Add(s));
+        }
+    }
+}
---
+++ Middlewares/RedirectUnsupportedCultures.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Localization.Routing;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Options;
+
+namespace i18n.Middlewares
+{
+    public class RedirectUnsupportedCultures
+    {
+        private readonly RequestDelegate _next;
+        private readonly string _routeDataStringKey;
+        private readonly IList<CultureInfo> _cultureItems;
+
+        public RedirectUnsupportedCultures(
+        RequestDelegate next, IOptions<RequestLocalizationOptions> options)
+        {
+            _next = next;
+            var provider = options.Value.RequestCultureProviders
+            .Select(x => x as RouteDataRequestCultureProvider)
+            .FirstOrDefault(x => x != null);
+
+            _cultureItems = options.Value.SupportedUICultures;
+
+            _routeDataStringKey = provider.RouteDataStringKey;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var requestedCulture = context.GetRouteValue(_routeDataStringKey)?.ToString();
+            var cultureFeature = context.Features.Get<IRequestCultureFeature>();
+
+            var actualCulture = cultureFeature?.RequestCulture.Culture.Name;
+
+            if(string.IsNullOrEmpty(requestedCulture) || _cultureItems.All(x => x.Name != requestedCulture)
+            && !string.Equals(requestedCulture, actualCulture, StringComparison.OrdinalIgnoreCase))
+        {
+                var newCulturedPath = GetNewPath(context, actualCulture);
+                context.Response.Redirect(newCulturedPath);
+                return;
+            }
+
+            await _next.Invoke(context);
+        }
+
+        private string GetNewPath(HttpContext context, string newCulture)
+        {
+            var routeData = context.GetRouteData();
+            var router = routeData.Routers[0]; // Does not exist :(
+            var virtualPathContext = new VirtualPathContext(context, routeData.Values, new RouteValueDictionary {
+                { _routeDataStringKey, newCulture }
+            });
+
+            return router.GetVirtualPath(virtualPathContext).VirtualPath;
+        }
+    }
+}
