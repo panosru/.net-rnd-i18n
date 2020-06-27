e4cbbda3abf39fdb56df9a13ca207bd293ad5007
Panagiotis <400362+panosru@users.noreply.github.com>
2020-06-27 13:37:39 +0300

    Use Linq on CustomCultureRouteModelConvention

--- i18n/Localization/CustomCultureRouteModelConvention.cs
+++ i18n/Localization/CustomCultureRouteModelConvention.cs
@@ -1,47 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
 namespace i18n.Localization
 {
     public class CustomCultureRouteModelConvention : IPageRouteModelConvention
     {
-        //public void Apply(PageRouteModel model)
-        //{
-        //    (from SelectorModel in model.Selectors
-        //     select new SelectorModel
-        //     {
-        //         AttributeRouteModel = new AttributeRouteModel
-        //         {
-        //             Template = AttributeRouteModel.CombineTemplates("/{culture:required}",
-        //                SelectorModel.AttributeRouteModel.Template)
-        //         }
-        //     })
-        //     .ToList()
-        //     .ForEach(s => model.Selectors.Add(s));
-        //}
-
         public void Apply(PageRouteModel model)
         {
-            var selectorCount = model.Selectors.Count;
-
-            for (var i = 0; i < selectorCount; i++)
-            {
-                var selector = model.Selectors[i];
-
-                model.Selectors.Add(new SelectorModel
-                {
-                    AttributeRouteModel = new AttributeRouteModel
-                    {
-                        Order = -1,
-                        Template = AttributeRouteModel.CombineTemplates(
-                              "{culture}", selector.AttributeRouteModel.Template),
-                    }
-                });
-
-                //selector.AttributeRouteModel.SuppressLinkGeneration = true;
-            }
+            (from SelectorModel in model.Selectors
+             select new SelectorModel
+             {
+                 AttributeRouteModel = new AttributeRouteModel
+                 {
+                     Order = -1,
+                     Template = AttributeRouteModel.CombineTemplates("/{culture:required}",
+                        SelectorModel.AttributeRouteModel.Template)
+                 }
+             })
+             .ToList()
+             .ForEach(s => model.Selectors.Add(s));
         }
     }
 }
