8cf29ae6ca1a47c9c6d9dca4137d040891b10e69
Panagiotis <400362+panosru@users.noreply.github.com>
2020-06-30 02:02:31 +0300

    Added Areas and ViewComponent examples (localized)

---
+++ i18n/ViewComponents/WeatherViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace i18n.ViewComponents
+{
+    public class WeatherViewComponent : ViewComponent
+    {
+        private double Temperature;
+
+        public WeatherViewComponent()
+        {
+            Random rand = new Random();
+            Temperature = rand.Next(-14, 42);
+
+            if("en-US" == CultureInfo.CurrentCulture.Name)
+            {
+                Temperature = Temperature * 1.8 + 32;
+            }
+
+            Temperature = Math.Round(Temperature, 0);
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("Default", (object)Temperature);
+        }
+    }
+}
